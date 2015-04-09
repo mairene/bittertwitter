1cf8ec2382090b41246ae2c2ac5c6f3243f927ce
Leandra Kim & Rafael Auriemo <pair+lea3182.rauriemo@devbootcamp.com>
2015-04-09 16:57:11 -0700

    completed initial draft user CRUD

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,5 +1,5 @@
+#login from index with handle and password validation
 get "/user" do
-  p params
   @handle = params[:handle]
   @password = params[:password]
   @user = User.where(handle: @handle).first
@@ -16,6 +16,7 @@ get "/user" do
   end
 end
 
+#from index create new user
 get "/users/new" do
   @email = params[:new_email]
   @password = params[:new_password]
@@ -28,12 +29,38 @@ get "/users/new" do
   end
 end
 
+#display user's edit page
+get "/users/:id" do
+  @id = params[:id].to_i
+  @user = User.where(id: @id).first
+  @unique_handle = params[:unique_handle]
+  erb :"user_views/edit"
+end
+
+#edit user
+put "/users/:id" do
+  new_handle = params[:handle]
+  new_email = params[:email]
+  @id = params[:id]
+  @user = User.where(id: @id).first
+  @user.email = new_email
+  @user.handle = new_handle
+  if @user.save
+    erb :"user_views/profile_page"
+  else
+    redirect "/users/#{@id}?unique_handle=false"
+  end
+end
+
+#delete user
 delete "/users/:id" do
   @id = params[:id].to_i
   @user = User.where(id: @id).first
   @user.destroy
   redirect "/"
 end
+
+
 
 
 
